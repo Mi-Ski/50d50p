c9a4f666dd4c94100a8e7ff3d9d4db0980596dcf
MiSki <skibamichal@interia.pl>
2021-11-08 01:07:52 +0100

    a

--- 2_progress-steps/script.js
+++ 2_progress-steps/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const progress = document.querySelector('#progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circles = document.querySelectorAll('.circle');
+
+let currActive = 1;
+
+next.addEventListener('click', () => {
+	currActive++;
+
+	if (currActive > circles.length) {
+		currActive = circles.length;
+	}
+	update();
+})
+
+
+prev.addEventListener('click', () => {
+	currActive--;
+
+	if (currActive < 1) {
+		currActive = 1;
+	}
+	update();
+})
+
+const update = () => {
+	circles.forEach((el, i) => {
+		if(i < currActive) {
+			el.classList.add("active");
+		} else {
+			el.classList.remove("active");
+		}
+	})
+
+	const actives = document.querySelectorAll('.active');
+	console.log(actives.length, circles.length);
+
+	progress.style.width = `${((actives.length -1)  / (circles.length -1)) * 100}%`;
+
+	if(currActive === 1) {
+		prev.disabled = true;
+	}
+	else if (currActive === circles.length) {
+		next.disabled = true;
+	}
+	else {
+		prev.disabled = next.disabled = false;
+	}
+}
